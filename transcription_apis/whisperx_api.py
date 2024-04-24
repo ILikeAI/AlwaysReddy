7ab3b6484751579a4efed073fa94d3faf4bea3da
kaminoer <yavriel@gmail.com>
2024-04-25 00:20:40 +0200

    print model from config

--- transcription_apis/whisperx_api.py
+++ transcription_apis/whisperx_api.py
@@ -10,7 +10,7 @@ class WhisperXClient:
         self.compute_type = "int8"
         self.language = config.TRANSCRIPTION_LANGUAGE
         self.model = wx.load_model(config.WHISPER_MODEL, self.device, language=self.language, compute_type=self.compute_type, download_root=config.WHISPER_MODEL_PATH)
-        print(f"Using WhisperX model: {self.model} and device: {self.device}")
+        print(f"Using WhisperX model: {config.WHISPER_MODEL} and device: {self.device}")
 
     def transcribe_audio_file(self, file_path):
         print(f"Transcribing audio file: {file_path}")
