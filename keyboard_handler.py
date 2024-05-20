e749172abcaaf7e5c254556b5ea02883aafaba0d
Jobus <johan.tylon@gmail.com>
2024-05-20 21:12:05 +0200

    Add support for held hotkeys

--- keyboard_handler.py
+++ keyboard_handler.py
@@ -45,8 +45,22 @@ class KeyboardHandler:
         raise NotImplementedError("start method must be implemented in subclasses")
 
 class KeyboardLibraryHandler(KeyboardHandler):
+    def __init__(self, verbose=False):
+        super().__init__(verbose)
+        self.held_hotkeys = {}
+
     def add_hotkey(self, hotkey, callback):
         keyboard.add_hotkey(hotkey, callback, suppress=config.SUPPRESS_NATIVE_HOTKEYS)
+
+    def add_held_hotkey(self, hotkey, callback):
+        self.held_hotkeys[hotkey] = False
+        keyboard.add_hotkey(hotkey, lambda: self.handle_held_callback(hotkey, callback, is_pressed=True), suppress=config.SUPPRESS_NATIVE_HOTKEYS)
+        keyboard.add_hotkey(hotkey, lambda: self.handle_held_callback(hotkey, callback, is_pressed=False), suppress=config.SUPPRESS_NATIVE_HOTKEYS, trigger_on_release=True)
+
+    def handle_held_callback(self, hotkey, callback, is_pressed):
+        if is_pressed != self.held_hotkeys[hotkey]:
+            callback(is_pressed=is_pressed)
+            self.held_hotkeys[hotkey] = is_pressed
 
     def start(self):
         try:
