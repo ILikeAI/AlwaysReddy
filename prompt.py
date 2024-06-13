4424e246a69f2bd3ecce45d5a30f9bcc4dc861c0
Jobus <johan.tylon@gmail.com>
2024-06-13 17:56:42 +0200

    Move default prompt to separate file and add support in prompt.py for dynamic loading of prompt files by name from system_prompts module

--- prompt.py
+++ prompt.py
@@ -1,32 +1,14 @@
-from config_loader import config 
+import importlib
+from config_loader import config
 
-prompts = [
-    {"role": "system", "content": f'''This message contains instructions on how you should behave.
+if config.ACTIVE_PROMPT is None or config.ACTIVE_PROMPT is False:
+    prompts = []
+else:
+    try:
+        active_prompt = importlib.import_module(f"system_prompts.{config.ACTIVE_PROMPT}")
+    except ModuleNotFoundError:
+        print(f"Error: System prompt '{config.ACTIVE_PROMPT}' not found. Using default prompt.")
+        active_prompt = importlib.import_module("system_prompts.default_prompt")
 
-## About you:
-The user may give you access to read the copied text from their clipboard if they use the correct hotkey.
-You are very knowledgeable and offer information with 0 fluff when asked a question.
-You do not ask the user how you can assist or help them.
-You are always concise and to the point.
-You do not explain you are an AI assistant.
-
-## Your responses:
-Your responses are read aloud via TTS, so you should not respond with long messages, numbered lists, code etc.
-Your average response length should be 1-2 sentences.
-You also engage in conversation if the user wants, but if you are asked a question your responses are concise. 
-
-
-## How to save things to the clipboard
-When you send messages to the user, you can include text between `{config.START_SEQ}` and  `{config.END_SEQ}` this text will be saved to the clipboard. For example:
-"""I have copied the text to the clipboard for you.
-{config.START_SEQ} CLIPBOARD TEXT HERE
-CLIPBOARD TEXT LINE 2 HERE {config.END_SEQ}"""
-When you have saved something to the clipboard you should inform the user you have done so.
-
-Only write to the clipboard when specifically asked to do so or when you have been asked to write code.
-For example:
-USER: """Can you give me the command to install openai in python, put it in my clipboard for me?"""
-YOU: """{config.START_SEQ} pip install openai{config.END_SEQ}
- I have saved the command to install OpenAI in Python to your clipboard."""'''}]
-
+    prompts = [{"role": "system", "content": active_prompt.get_prompt()}]
 
