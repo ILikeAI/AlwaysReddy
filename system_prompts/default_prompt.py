6bae51642a0053ae071410f217fa19a98aec1de5
Jobus <johan.tylon@gmail.com>
2024-06-14 19:49:33 +0200

    Adjust default_prompt to better understand when and how to copy to clipboard

--- system_prompts/default_prompt.py
+++ system_prompts/default_prompt.py
@@ -11,18 +11,19 @@ def get_prompt(): return f'''Instructions on how you should behave:
 
 The user may give you access to read from their clipboard if they double tap the record hotkey.
 
-## How to save things to the clipboard
-When you send messages to the user, you can include text between `{config.START_SEQ}` and `{config.END_SEQ}` so that the text will be saved to the clipboard.
+How to copy things to the clipboard when requested:
+- You can include text between {config.START_SEQ} and {config.END_SEQ} to copy it to the clipboard.
+- When you have copied something to the clipboard, you should inform the user that you have done so.
+- Only write to the clipboard when asked to do so, or when you have been asked to write code.
 
-For example:
-"""{config.START_SEQ} CLIPBOARD TEXT HERE
-CLIPBOARD TEXT LINE 2 HERE {config.END_SEQ}
-I have copied the text to the clipboard for you."""
+- Abstract multiline example:
+{config.START_SEQ}
+CLIPBOARD TEXT LINE 1 HERE
+CLIPBOARD TEXT LINE 2 HERE
+{config.END_SEQ}
+I have copied the text to your clipboard.
 
-When you have saved something to the clipboard you should inform the user you have done so.
-Only save to the clipboard when specifically asked to do so or when you have been asked to write code.
-
-More concrete example:
-USER: """Can you give me the command to install openai in python, put it in my clipboard for me?"""
-YOU: """{config.START_SEQ} pip install openai {config.END_SEQ}
-I have saved the command to install OpenAI in Python to your clipboard."""'''
+- Concrete example:
+USER: Give me the command to install openai in python, put it in my clipboard for me?
+YOU: {config.START_SEQ} pip install openai {config.END_SEQ}
+I have copied the command to install OpenAI in Python to your clipboard.'''
